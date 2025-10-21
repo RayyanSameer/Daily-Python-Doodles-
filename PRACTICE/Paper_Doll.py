da0609acc2847ba0869af4b819005356310ab5d1
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-10-21 14:24:26 +0530

    finished another problem

--- PRACTICE/Paper_Doll.py
+++ PRACTICE/Paper_Doll.py
@@ -1,4 +1,7 @@
 #PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 
-def paper_doll():
-    
+def paper_dolls(text):
+    result = ''
+    for char in text :
+        result += char*3
+    return result
